fe7dd9d3364818a54efd414c7c137416d1d1632e
shallashuch <massa.ilaria.96@gmail.com>
2024-01-07 11:01:46 +0100

    last maybe

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let openButton = document.querySelector('.icon-open');
 let closeButton = document.querySelector('.icon-close');
 let menu = document.querySelector('.menu-items');
 
+
 openButton.addEventListener('click', displayMenu);
 closeButton.addEventListener('click', closeMenu);
-window.addEventListener('load', checkWindowWidth);
-window.addEventListener('resize', checkWindowWidth);
 
 function displayMenu () {
   openButton.style.display = 'none';
@@ -19,22 +18,5 @@ function closeMenu (callback) {
   openButton.style.display = 'block';
   menu.classList.remove('active');
   menu.classList.add('inactive');
-
-  setTimeout(() => {
-    menu.classList.remove('inactive');
-    menu.classList.add('menu-items');
-  }, 300);
-}
-
-function checkWindowWidth() {
-  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var maxWidth = 700;
-
-  if (windowWidth > maxWidth) {
-    openButton.style.display = 'none';
-    menu.classList.remove('inactive');
-  } else {
-    openButton.style.display = 'block';
-  }
 }
 
